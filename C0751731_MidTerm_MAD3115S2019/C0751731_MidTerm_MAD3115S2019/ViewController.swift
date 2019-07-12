3a2dfee0498a37957f0bcea08cbfa41de01bf5d7
Shivani Dhiman <shivanidhiman1619@gmail.com>
2019-07-11 20:16:08 -0400

    [Shivani] : Initial Commit

---
+++ C0751731_MidTerm_MAD3115S2019/C0751731_MidTerm_MAD3115S2019/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  C0751731_MidTerm_MAD3115S2019
+//
+//  Created by Vijender Singh on 2019-07-11.
+//  Copyright © 2019 Shivani Dhiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
