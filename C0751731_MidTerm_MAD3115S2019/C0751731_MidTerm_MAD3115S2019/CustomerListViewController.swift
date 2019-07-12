0d62ae0e87dc825bc00e41b3664bf6072b5cdd11
Shivani Dhiman <shivanidhiman1619@gmail.com>
2019-07-12 13:12:38 -0400

    Customization of Customer List

--- C0751731_MidTerm_MAD3115S2019/C0751731_MidTerm_MAD3115S2019/CustomerListViewController.swift
+++ C0751731_MidTerm_MAD3115S2019/C0751731_MidTerm_MAD3115S2019/CustomerListViewController.swift
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class CustomerListViewController: UITableViewController {
+class CustomerListViewController: UITableViewController,UITableViewDelegate , UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
+    var customerList = [CustomersStruct]()
+    var customerArray = Array<Customer>()
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
